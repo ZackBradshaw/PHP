cf067ab0febab37ece29f8144c0640ce73d64da7
zack <zackzbradshaw@gmail.com>
2022-12-15 14:59:07 -0600

    Created dynamic active nav styling

--- about.php
+++ about.php
@@ -1,5 +1,7 @@
 <?php
 
+require 'views/partials/functions.php';
+
 $heading = "About Us";
 
 require "views/about.view.php";
--- index.php
+++ index.php
@@ -1,5 +1,7 @@
 <?php
 
+require 'views/partials/functions.php';
+
 $heading = "Home Page";
 
 require "views/index.view.php";
