2e105d6f8dd768853be2c95b21af18d8a16052c5
zack <zackzbradshaw@gmail.com>
2022-12-15 18:18:21 -0600

    Created Routes

--- controllers/about.php
+++ controllers/about.php
@@ -1,7 +1,5 @@
 <?php
 
-require 'views/partials/functions.php';
-
 $heading = "About Us";
 
 require "views/about.view.php";
--- controllers/contact.php
+++ controllers/contact.php
@@ -1,7 +1,5 @@
 <?php
 
-require 'views/partials/functions.php';
-
 $heading = "Contact Us";
 
 require "views/contact.view.php";
